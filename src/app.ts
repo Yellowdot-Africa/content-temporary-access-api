1de99924261a443c9d121f22484d18e1d763151f
yousuf.glb <yousuf.glb@gmail.com>
2025-08-26 14:27:02 +0530

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { openapiSpec } from "./docs/openapi";
 import routes from "./routes";
+import cors from "cors";   
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Swagger docs
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpec));
